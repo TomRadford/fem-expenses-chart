7159c53c0aa079f1daee6889fe2d64749d20cd72
Tom Radford <tom@theradford.com>
2022-07-04 19:29:54 +0200

    Skeleton

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+const App = () => {
+  return (
+    <div className="App">
+       My balance
+  $921.48
+
+  Spending - Last 7 days
+
+  mon
+  tue
+  wed
+  thu
+  fri
+  sat
+  sun
+
+  Total this month
+  $478.33
+
+  +2.4%
+  from last month
+
+    </div>
+  )
+}
+
+export default App;
