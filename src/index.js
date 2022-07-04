ec82ed4f167b24c5b01f6fb1c17457c7b21d2020
Tom Radford <tom@theradford.com>
2022-07-04 19:56:42 +0200

    feat: fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './scss/style.scss';
+import './styles/style.scss';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
