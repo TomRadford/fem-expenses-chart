3394540d7633c2fe05af5531170a87c8dd754fb2
Tom Radford <tom@theradford.com>
2022-07-06 12:42:06 +0200

    wip: block styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 const App = () => {
   return (
-    <div className="grid">
-      <aside className="grid__aside">
-        <p>My balance</p>
-        <h3>$921.48</h3>
-      </aside>
-      <main className="grid__main">
-        <h3>Spending - Last 7 days</h3>
-      </main>
+    <div className="flex">
+      <div className="grid">
+        <aside className="grid__aside">
+          <div className="balance">
+            <div>
+              <p>My balance</p>
+              <h2>$921.48</h2>
+            </div>
+            <div className="circles">
+              <div className="circle-one"></div>
+              <div className="circle-two"></div>
+            </div>
+          </div>
+        </aside>
+        <main className="grid__main">
+          <div className="stats">
+            <h3>Spending - Last 7 days</h3>
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
